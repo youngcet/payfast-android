8daa8cc30f9a580cc100834674dccc3e3ff6d4ec
Cedric <young.cet@gmail.com>
2025-01-16 20:51:14 +0200

    bug fixing

--- src/main/java/za/co/permanentlink/payfast/PayFastActivity.java
+++ src/main/java/za/co/permanentlink/payfast/PayFastActivity.java
@@ -2,7 +2,6 @@ package za.co.permanentlink.payfast;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -49,7 +48,7 @@ public abstract class PayFastActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_pay_fast);
 
         FlutterEngine flutterEngine=new FlutterEngine(this);
